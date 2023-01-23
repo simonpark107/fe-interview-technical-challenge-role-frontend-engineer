53276eaa7d4cde66b05df0e865b35ce6c8d7f26c
Simon Park <simonpark107@gmail.com>
2023-01-22 20:00:13 -0800

    Updated App with QueryClientProvider and policyholders route

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,38 @@ import TableDemoView from './components/TableDemoView';
 import HomeView from './components/HomeView';
 import RedirectView from './components/RedirectView';
 import YouCanDoItView from './components/YouCanDoItView';
+import PolicyholdersView from './components/PolicyholdersView';
 import Layout from './components/Layout';
 import { useState } from 'react';
 import Modal from './components/Modal';
 import Instructions from './components/Instructions';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './react-query/queryClient';
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <BrowserRouter>
-      <Layout onFooterClick={() => setIsModalOpen(true)}>
-        <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/table" element={<TableDemoView />} />
-          <Route path="/you-can-do-it" element={<YouCanDoItView />} />
-          <Route path="*" element={<RedirectView />} />
-        </Routes>
-      </Layout>
-      <Modal
-        isOpen={isModalOpen}
-        handleClose={() => setIsModalOpen(false)}
-        title="Sure Technical Challenge"
-      >
-        <Instructions />
-      </Modal>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Layout onFooterClick={() => setIsModalOpen(true)}>
+          <Routes>
+            <Route path="/" element={<HomeView />} />
+            <Route path="/policyholders" element={<PolicyholdersView />} />
+            <Route path="/table" element={<TableDemoView />} />
+            <Route path="/you-can-do-it" element={<YouCanDoItView />} />
+            <Route path="*" element={<RedirectView />} />
+          </Routes>
+        </Layout>
+        <Modal
+          isOpen={isModalOpen}
+          handleClose={() => setIsModalOpen(false)}
+          title="Sure Technical Challenge"
+        >
+          <Instructions />
+        </Modal>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
